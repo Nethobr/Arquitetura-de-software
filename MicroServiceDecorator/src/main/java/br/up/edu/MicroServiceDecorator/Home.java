c6b4c18f11d91ce41d2fdcf068afc2bed8cdbfa7
Nethobr <>
2022-11-10 19:34:00 -0300

    ResquestMapping

--- MicroServiceDecorator/src/main/java/br/up/edu/MicroServiceDecorator/Home.java
+++ MicroServiceDecorator/src/main/java/br/up/edu/MicroServiceDecorator/Home.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//	Bibliotecas
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //	Projeto
@@ -23,7 +24,7 @@ import br.up.edu.MicroServiceDecorator.models.CoffeModel;
 public class Home 
 {
 	@GetMapping("coffe")
-	public ResponseEntity<CoffeModel> coffeModel() 
+	public ResponseEntity<CoffeModel> coffe() 
 	{
 		Coffe 	cafe = new BasicCoffe();
 				cafe = new LeiteDecorator(cafe);
@@ -50,10 +51,17 @@ public class Home
 	}
 
 	@GetMapping("coffes")
-	public ResponseEntity<ArrayList<CoffeModel>> aEntity ()
+	public ResponseEntity<ArrayList<CoffeModel>> coffes()
 	{
-		cafesModel();
+		// cafesModel();
 		return new ResponseEntity<ArrayList<CoffeModel>>(coffesModel, HttpStatus.OK);
+	}
+
+	@PostMapping("coffe")
+	public ResponseEntity<CoffeModel> addCoffe(@RequestBody CoffeModel coffe)
+	{
+		coffesModel.add(coffe);
+		return new ResponseEntity<CoffeModel>(coffe, HttpStatus.OK);
 	}
 
 	@GetMapping("/")
@@ -62,13 +70,5 @@ public class Home
 		return "Olá!" +
 				"<br/>" +
 				"Bem vindo ao nosso micro-serviço!";
-	}
-
-	@GetMapping("/error")
-	public @ResponseBody String error()
-	{
-		return "Olá!" +
-				"<br/>" +
-				"Ocorreu um erro!.";
 	}
 }	//	Home
